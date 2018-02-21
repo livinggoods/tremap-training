b218c877120443ae56c3be9fb0c9a080e99678fc
David Kimaru <kimarudg@gmail.com>
2018-02-21 14:56:59 +0300

    Add cohorts

--- app/src/main/java/com/expansion/lg/kimaru/training/objs/Cohort.java
+++ app/src/main/java/com/expansion/lg/kimaru/training/objs/Cohort.java
@@ -11,5 +11,55 @@ public class Cohort {
     Integer branchId;
     boolean archived;
 
+    public Cohort() {
+    }
 
+    public Cohort(Integer id, String cohortNumber, String cohortName, Integer branchId,
+                  boolean archived) {
+        this.id = id;
+        this.cohortNumber = cohortNumber;
+        this.cohortName = cohortName;
+        this.branchId = branchId;
+        this.archived = archived;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCohortNumber() {
+        return cohortNumber;
+    }
+
+    public void setCohortNumber(String cohortNumber) {
+        this.cohortNumber = cohortNumber;
+    }
+
+    public String getCohortName() {
+        return cohortName;
+    }
+
+    public void setCohortName(String cohortName) {
+        this.cohortName = cohortName;
+    }
+
+    public Integer getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(Integer branchId) {
+        this.branchId = branchId;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
 }
