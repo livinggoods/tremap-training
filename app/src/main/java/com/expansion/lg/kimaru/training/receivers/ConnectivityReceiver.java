38a6dbd1b42a1962f31f53125f4661fe25547a72
David Kimaru <kimarudg@gmail.com>
2018-02-23 07:21:50 +0300

    Add ConnectivityReceiver

---
+++ app/src/main/java/com/expansion/lg/kimaru/training/receivers/ConnectivityReceiver.java
@@ -0,0 +1,41 @@
+package com.expansion.lg.kimaru.training.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by kimaru on 2/23/18.
+ */
+
+public class ConnectivityReceiver extends BroadcastReceiver {
+
+    public static ConnectivityReceiverListener connectivityReceiverListener;
+
+    public ConnectivityReceiver(){
+        super();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null & activeNetwork.isConnectedOrConnecting();
+        if(connectivityReceiverListener != null){
+            connectivityReceiverListener.onNetworkConnectionChanged(isConnected);
+        }
+    }
+
+    public static boolean isConnected(){
+        ConnectivityManager cm = (ConnectivityManager) TremapApp.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    public interface ConnectivityReceiverListener {
+        void onNetworkConnectionChanged(boolean isConnected);
+    }
+
+}
