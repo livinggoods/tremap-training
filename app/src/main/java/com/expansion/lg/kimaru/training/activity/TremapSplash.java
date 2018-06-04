f9896e569e1afd8e2e8362411debf0021a254502
David Kimaru <kimarudg@gmail.com>
2018-06-04 12:24:53 +0300

    Spalsh Screen

---
+++ app/src/main/java/com/expansion/lg/kimaru/training/activity/TremapSplash.java
@@ -0,0 +1,14 @@
+package com.expansion.lg.kimaru.training.activity;
+
+/**
+ * Created by kimaru on 5/8/18 using Android Studio.
+ * Maintainer: David Kimaru
+ *
+ * @github https://github.com/kimarudg
+ * @email kimarudg@gmail.com
+ * @phone +254722384549
+ * @web gakuu.co.ke
+ */
+
+public class TremapSplash {
+}
